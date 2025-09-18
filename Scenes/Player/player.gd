b64d4affd53cd554bdfc6d7762d357ebee2382f5
M D <mgcusr@gmail.com>
2025-09-17 19:32:53 -0700

    001_testing_git_changes

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+class_name Player
+
+@export var fallen_off_y: float = 500.0
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var debug_label: Label = $DebugLabel
+
+const GRAVITY: float = 690.0
+const JUMP_SPEED: float = -270.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 350.0
+
+
+func _ready() -> void:
+	pass
+
+ 
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_SPEED
+
+	velocity.x = RUN_SPEED * Input.get_axis("left", "right")
+	if not is_equal_approx(velocity.x, 0.0):
+		sprite_2d.flip_h = true if velocity.x < 0 else false
+	
+	velocity.y = clampf(velocity.y, JUMP_SPEED, MAX_FALL)
+	
+	move_and_slide()
+	update_debug_label()
+	fallen_off()
+
+func update_debug_label() -> void:
+	var debug_str: String = ""
+	debug_str += "Floor: %s\n" % [is_on_floor()]
+	debug_str += "V: %1.f, %1.f\n" % [velocity.x, velocity.y]
+	debug_str += "P: %1.f, %1.f" % [global_position.x, global_position.y]
+	debug_label.text = debug_str
+
+
+func fallen_off() -> void:
+	if global_position.y  > fallen_off_y:
+		queue_free()
+	pass
