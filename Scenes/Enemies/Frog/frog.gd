834f45af38f0ae7d8c1bb0cc0746bf66dd43edad
w <mgcusr@gmail.com>
2025-09-19 20:32:51 -0700

    144 eagle start

--- Scenes/Enemies/Frog/frog.gd
+++ Scenes/Enemies/Frog/frog.gd
@@ -1,14 +1,27 @@
 extends EnemyBase
 
+
+@onready var jump_timer: Timer = $JumpTimer
+
 var _seen_player: bool = false
 var _can_jump: bool = false
+
+const JUMP_VELOCITY_R: Vector2 = Vector2(100, -150)
+const JUMP_VELOCITY_L: Vector2 = Vector2(-100, -150)
+
+
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
 	velocity.y += delta * _gravity
-
+	
+	apply_jump()
 	move_and_slide()
 	flip_me()
+	
+	if is_on_floor():
+			velocity.x = 0
+			animated_sprite_2d.play('idle')
 
 
 func flip_me() -> void:
@@ -19,10 +32,26 @@ func flip_me() -> void:
 
 
 func apply_jump() -> void:
-	if not is_on_floor() or not _can_jump or not _seen_player:
+	if is_on_floor() == false or _can_jump == false:
 		return
-	
+
+	if _seen_player == false:
+		return
 		
+	velocity = JUMP_VELOCITY_R if animated_sprite_2d.flip_h else JUMP_VELOCITY_L
+	_can_jump = false
+	start_timer()
+	animated_sprite_2d.play("jump")
+
+func start_timer() -> void:
+	jump_timer.wait_time = randf_range(2.0, 3.0)
+	jump_timer.start()
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	if _seen_player == false:
+		_seen_player = true
+		start_timer()
 
 func _on_jump_timer_timeout() -> void:
 	_can_jump = true
