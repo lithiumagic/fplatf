41ea0e36bc44d9fcaa4e980740bca2f18c52d7c1
w <mgcusr@gmail.com>
2025-10-03 00:00:34 -0700

    155. Player invincible start

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,21 +2,27 @@ extends CharacterBody2D
 
 class_name Player
 
+
+const DAMAGE = preload("res://assets/sound/damage.wav")
+const JUMP = preload("res://assets/sound/jump.wav")
+
 @export var fallen_off_y: float = 500.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var debug_label: Label = $DebugLabel
 @onready var shooter: Shooter = $Shooter
 @onready var sound: AudioStreamPlayer2D = $Sound
+@onready var hurt_timer: Timer = $HurtTimer
 
 #const GRAVITY: float = 690.0
 #const JUMP_SPEED: float = -270.0
 const RUN_SPEED: float = 150.0
 const MAX_FALL: float = 350.0
-
 const GRAVITY: float = 2000.0  # increased
 const JUMP_SPEED: float = -540.0  # increased
+const HURT_JUMP_VELOCITY: Vector2 = Vector2(0, -130.0)
 
+var is_hurt: bool = false
 
 
 func _ready() -> void:
@@ -30,9 +36,27 @@ func _enter_tree() -> void:
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	
+	get_input()
+
+	velocity.y = clampf(velocity.y, JUMP_SPEED, MAX_FALL)
+	
+	move_and_slide()
+	update_debug_label()
+	fallen_off()
+
+
+func play_effect(effect: AudioStream) -> void:
+	sound.stop()
+	sound.stream = effect
+	sound.play()
+
+
+func get_input() -> void:
+	if is_hurt == true:
+		return
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_SPEED
-		sound.play()
+		play_effect(JUMP)
 	
 	# wall jump
 	if is_on_wall() and Input.is_action_just_pressed("jump"):
@@ -49,11 +73,7 @@ func _physics_process(delta: float) -> void:
 	if not is_equal_approx(velocity.x, 0.0):
 		sprite_2d.flip_h = true if velocity.x < 0 else false
 	
-	velocity.y = clampf(velocity.y, JUMP_SPEED, MAX_FALL)
-	
-	move_and_slide()
-	update_debug_label()
-	fallen_off()
+
 
 func update_debug_label() -> void:
 	var debug_str: String = ""
@@ -67,3 +87,20 @@ func fallen_off() -> void:
 	if global_position.y  > fallen_off_y:
 		queue_free()
 	pass
+
+func apply_hurt_jump() -> void:
+	is_hurt = true
+	velocity = HURT_JUMP_VELOCITY
+	play_effect(DAMAGE)
+	hurt_timer.start()
+
+func apply_hit() -> void:
+	apply_hurt_jump()
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	call_deferred("apply_hit")
+	
+
+
+func _on_hurt_timer_timeout() -> void:
+	is_hurt = false
