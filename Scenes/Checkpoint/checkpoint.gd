c6ee3269422c0ec30d141625759a206f9b142e2c
w <mgcusr@gmail.com>
2025-10-07 22:07:00 -0700

    168. player lives

--- Scenes/Checkpoint/checkpoint.gd
+++ Scenes/Checkpoint/checkpoint.gd
@@ -11,3 +11,12 @@ func _ready() -> void:
 
 func boss_killed() -> void:
 	_boss_killed = true
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "open":
+		set_deferred("monitoring", true)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("level complete")
